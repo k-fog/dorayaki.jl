f1dc66cdfd6e32f39b92bdcd501617d9aae8d633
k-fog <59814110+k-fog@users.noreply.github.com>
2021-05-25 18:41:29 +0900

    style: add empty line

--- src/basicmath.jl
+++ src/basicmath.jl
@@ -5,8 +5,11 @@ import Base.Broadcast: broadcasted
     Add <: Func
 """
 @func mutable struct Add end
+
 forward(f::Add, A, B) = A .+ B
+
 add(A, B) = Add()(A, B)
+
 +(A::Var, B::Var) = add(A, B)
 +(A::Var, B) = add(A, B)
 +(A, B::Var) = add(A, B)
@@ -16,8 +19,11 @@ add(A, B) = Add()(A, B)
     Mul <: Func
 """
 @func mutable struct Mul end
+
 forward(f::Mul, A, B) = A .* B
+
 mul(A, B) = Mul()(A, B)
+
 broadcasted(::typeof(*), A::Var, B::Var) = mul(A, B)
 broadcasted(::typeof(*), A::Var, B) = mul(A, B)
 broadcasted(::typeof(*), A, B::Var) = mul(A, B)
@@ -27,8 +33,11 @@ broadcasted(::typeof(*), A, B::Var) = mul(A, B)
     Neg <: Func
 """
 @func mutable struct Neg end
+
 forward(f::Neg, x) = -x
+
 neg(x) = Neg()(x)
+
 -(x::Var) = neg(x)
 
 
@@ -36,8 +45,11 @@ neg(x) = Neg()(x)
     Sub <: Func
 """
 @func mutable struct Sub end
+
 forward(f::Sub, A, B) = A .- B
+
 sub(A, B) = Sub()(A, B)
+
 -(A::Var, B::Var) = sub(A, B)
 -(A::Var, B) = sub(A, B)
 -(A, B::Var) = sub(A, B)
@@ -50,17 +62,37 @@ sub(A, B) = Sub()(A, B)
     c::T
     Pow(c::T) where T = new{T}(c)
 end
+
 forward(f::Pow, x) = x.^f.c
+
 pow(x, c) = Pow(c)(x)
+
 Base.:^(x::Var, c) = pow(x, c)
+
+
+"""
+    Div <: Func
+"""
+@func mutable struct Div end
+
+forward(f::Div, A, B) = A ./ B
+
+div(A, B) = Div()(A, B)
+
+/(A::Var, B::Var) = div(A, B)
+/(A::Var, B) = div(A, B)
+/(A, B::Var) = div(A, B)
 
 
 """
     MatMul <: Func
 """
 @func mutable struct MatMul end
-forward(f::MatMul, A::AbstractArray, B::AbstractArray) = A * B
+
+forward(f::MatMul, A, B) = A * B
+
 matmul(A, B) = MatMul()(A, B)
+
 *(A::Var, B::Var) = matmul(A, B)
 *(A::Var, B) = matmul(A, B)
 *(A, B::Var) = matmul(A, B)
